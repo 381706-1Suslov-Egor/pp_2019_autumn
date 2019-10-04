35793878338118f89b07222e22bdcb4347a977b2
381706-1Suslov-Egor <43166927+381706-1Suslov-Egor@users.noreply.github.com>
2019-10-04 13:57:19 +0300

    wow

--- modules/task_1/suslov_e_chislo_cheredovaniy/chislo_cheredovaniy.h
+++ modules/task_1/suslov_e_chislo_cheredovaniy/chislo_cheredovaniy.h
@@ -1,6 +1,6 @@
 // Copyright 2019 Suslov Egor
-#ifndef MODULES_TASK_1_CHISLO_CHEREDOVANIY_H_
-#define MODULES_TASK_1_CHISLO_CHEREDOVANIY_H_
+#ifndef MODULES_TASK_1_SUSLOV_E_CHISLO_CHEREDOVANIY_CHISLO_CHEREDOVANIY_H_
+#define MODULES_TASK_1_SUSLOV_E_CHISLO_CHEREDOVANIY_CHISLO_CHEREDOVANIY_H_
 
 #include <vector>
 #include <string>
@@ -9,5 +9,5 @@ std::vector<int> getRandomVector(int  sz);
 int getChisloCheredovaniy(std::vector<int> global_vec, int count_size_vector);
 int getParallelOperations(std::vector<int> global_vec, int count_size_vector);
 
-#endif  // MODULES_TASK_1_CHISLO_CHEREDOVANIY_H_
+#endif  // MODULES_TASK_1_SUSLOV_E_CHISLO_CHEREDOVANIY_CHISLO_CHEREDOVANIY_H_
 
