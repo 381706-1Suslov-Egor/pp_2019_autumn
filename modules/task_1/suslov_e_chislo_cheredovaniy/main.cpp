a985549593848a4c297630557bc050de73251c4e
381706-1Suslov-Egor <43166927+381706-1Suslov-Egor@users.noreply.github.com>
2019-10-04 16:01:50 +0300

    space_all3

--- modules/task_1/suslov_e_chislo_cheredovaniy/main.cpp
+++ modules/task_1/suslov_e_chislo_cheredovaniy/main.cpp
@@ -8,81 +8,81 @@
 TEST(Parallel_Operations_MPI, Test_on_primere_chetnom) {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
- std::vector<int> global_vec = { 2, -1, -2, -3, 5, 6, 7, 8 };
+  std::vector<int> global_vec = { 2, -1, -2, -3, 5, 6, 7, 8 };
   int ChisloCheredovaniy = getParallelOperations(global_vec, global_vec.size());
   ASSERT_EQ(ChisloCheredovaniy, 2);
 }
 
 TEST(Parallel_Operations_MPI, Test_on_primere_nechetnom) {
- int rank;
- MPI_Comm_rank(MPI_COMM_WORLD, &rank);
- std::vector<int> global_vec = { 2, -1, -2, -3, 5, 6, 7, 8, 9 };
- int ChisloCheredovaniy = getParallelOperations(global_vec, global_vec.size());
- ASSERT_EQ(ChisloCheredovaniy, 2);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> global_vec = { 2, -1, -2, -3, 5, 6, 7, 8, 9 };
+  int ChisloCheredovaniy = getParallelOperations(global_vec, global_vec.size());
+  ASSERT_EQ(ChisloCheredovaniy, 2);
 }
 
 TEST(Parallel_Operations_MPI, Test_values_positiv_or_null) {
- int rank;
- MPI_Comm_rank(MPI_COMM_WORLD, &rank);
- std::vector<int> global_vec;
- const int count_size_vector = 100;
- int ChisloCheredovaniy;
- if (rank == 0) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> global_vec;
+  const int count_size_vector = 100;
+  int ChisloCheredovaniy;
+  if (rank == 0) {
 		global_vec = getRandomVector(count_size_vector);
 		ChisloCheredovaniy = getParallelOperations(global_vec, global_vec.size());
- }
- if (rank == 0) {
+  }
+  if (rank == 0) {
 		ASSERT_GT(ChisloCheredovaniy, 0);
- }
+  }
 }
 
 TEST(Parallel_Operations_MPI, Test_on_rand_primere_chetnom) {
- int rank;
- MPI_Comm_rank(MPI_COMM_WORLD, &rank);
- std::vector<int> global_vec;
- const int count_size_vector = 100;
- int ChisloCheredovaniy;
- if (rank == 0) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> global_vec;
+  const int count_size_vector = 100;
+  int ChisloCheredovaniy;
+  if (rank == 0) {
 		global_vec = getRandomVector(count_size_vector);
 		ChisloCheredovaniy = getParallelOperations(global_vec, global_vec.size());
- }
- if (rank == 0) {
+  }
+  if (rank == 0) {
       ASSERT_GT(ChisloCheredovaniy, 0);
- }
+  }
 }
 
 TEST(Parallel_Operations_MPI, ) {
- int rank;
- MPI_Comm_rank(MPI_COMM_WORLD, &rank);
- std::vector<int> global_vec;
- const int count_size_vector = 101;
- int ChisloCheredovaniy;
- if (rank == 0) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> global_vec;
+  const int count_size_vector = 101;
+  int ChisloCheredovaniy;
+  if (rank == 0) {
 		global_vec = getRandomVector(count_size_vector);
 		ChisloCheredovaniy = getParallelOperations(global_vec, global_vec.size());
- }
- if (rank == 0) {
+  }
+  if (rank == 0) {
       ASSERT_GT(ChisloCheredovaniy, 0);
- }
+  }
 }
 
 TEST(Parallel_Operations_MPI, Test_sravneniye_chisla_cheredovaniy) {
- int rank;
- MPI_Comm_rank(MPI_COMM_WORLD, &rank);
- std::vector<int> global_vec, local_vec;
- const int count_size_vector = 100;
- int ChisloCheredovaniy1, ChisloCheredovaniy2;
- if (rank == 0) {
-	 local_vec = getRandomVector(count_size_vector);
-	 global_vec = local_vec;
-	 global_vec[0] = 1;
-	 global_vec[1] = -1;
-	 ChisloCheredovaniy2 = getParallelOperations(global_vec, global_vec.size());
-	 ChisloCheredovaniy1 = getParallelOperations(local_vec, local_vec.size());
- }
- if (rank == 0) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> global_vec, local_vec;
+  const int count_size_vector = 100;
+  int ChisloCheredovaniy1, ChisloCheredovaniy2;
+  if (rank == 0) {
+	  local_vec = getRandomVector(count_size_vector);
+	  global_vec = local_vec;
+	  global_vec[0] = 1;
+	  global_vec[1] = -1;
+	  ChisloCheredovaniy2 = getParallelOperations(global_vec, global_vec.size());
+	  ChisloCheredovaniy1 = getParallelOperations(local_vec, local_vec.size());
+  }
+  if (rank == 0) {
     ASSERT_GT(ChisloCheredovaniy2, ChisloCheredovaniy1);
- }
+  }
 }
 int main(int argc, char** argv) {
 	::testing::InitGoogleTest(&argc, argv);
