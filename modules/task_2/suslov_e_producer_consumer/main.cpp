eda8f44ff857c7871df0702d4a394973c9a1b1f1
381706-1Suslov-Egor <43166927+381706-1Suslov-Egor@users.noreply.github.com>
2019-11-17 02:44:14 +0300

    producer_consumer

--- modules/task_2/suslov_e_producer_consumer/main.cpp
+++ modules/task_2/suslov_e_producer_consumer/main.cpp
@@ -72,15 +72,17 @@ TEST(Producer_Consumer, Producer_Test3) {
         for (int i = 0; i < kol_resursov; i++) {
             Producer(buffer, kol_elem_in_buffer, rank, 1);
         }
-        for (int i = 0; i < kol_resursov; i++) {
-            ASSERT_EQ(buffer[i], 1);
+        if (rank == 0) {
+            for (int i = 0; i < kol_resursov; i++) {
+                ASSERT_EQ(buffer[i], 1);
+            }
         }
     } else {
-        for (int i = 0; i < kol_resursov; i++) {
-            Producer(buffer, kol_elem_in_buffer, kol_resursov % (size - 1), 2);
-        }
-        for (int i = 0; i < kol_resursov; i++) {
-            ASSERT_EQ(buffer[i], 2);
+        Producer(buffer, kol_elem_in_buffer, rank, 2);
+        if (rank == 0) {
+            for (int i = 0; i < kol_resursov; i++) {
+                ASSERT_EQ(buffer[i], 2);
+            }
         }
     }
 }
@@ -108,6 +110,7 @@ TEST(Producer_Consumer, Consumer_Test1) {
             ASSERT_EQ(1, resurce_consume[i]);
         }
     } else {
+        Producer(buffer, kol_elem_in_buffer, rank, 1);
         std::vector<int> resurce_consume1(kol_resursov, -1);
         int *resurce_consume;
         resurce_consume = Create_dinamic_massiv_from_vector(resurce_consume1);
@@ -178,14 +181,23 @@ TEST(Producer_Consumer, Consumer_Test3) {
             ASSERT_EQ(1, resurce_consume[i]);
         }
     } else {
-        for (int i = 0; i < kol_resursov; i++) {
-            Producer(buffer, kol_elem_in_buffer, kol_resursov % (size - 1), 2);
+        if(rank == 1) {
+            for (int i = 0; i < kol_resursov; i++) {
+                Producer(buffer, kol_elem_in_buffer, rank, 1);
+            }
         }
-        for (int i = 0; i < kol_resursov; i++) {
-            ASSERT_EQ(buffer[i], 2);
+        if (rank == 0) {
+            std::vector<int> resurce_consume1(kol_resursov, -1);
+            int *resurce_consume;
+            resurce_consume = Create_dinamic_massiv_from_vector(resurce_consume1);
+            for (int i = 0; i < kol_resursov; i++) {
+                Consumer(buffer, kol_elem_in_buffer, 0, &resurce_consume[i]);
+                ASSERT_EQ(1, resurce_consume[i]);
+            }
         }
     }
 }
+
 TEST(Producer_Consumer, Producer_Consumer_Test1) {
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -198,26 +210,29 @@ TEST(Producer_Consumer, Producer_Consumer_Test1) {
     int kol_resursov = 5;
     if (size == 1) {
         for (int i = 0; i < kol_resursov; i++) {
-            Producer(buffer, kol_elem_in_buffer, rank, 3);
+            Producer(buffer, kol_elem_in_buffer, rank, i);
         }
         std::vector<int> resurce_consume1(kol_resursov, -1);
         int *resurce_consume;
         resurce_consume = Create_dinamic_massiv_from_vector(resurce_consume1);
         for (int i = 0; i < kol_resursov; i++) {
             Consumer(buffer, kol_elem_in_buffer, 0, &resurce_consume[i]);
-            ASSERT_EQ(3, resurce_consume[i]);
+            ASSERT_EQ(i, resurce_consume[i]);
         }
     } else {
-        Producer(buffer, kol_elem_in_buffer, rank, 3);
-        for (int i = 0; i < size; i++) {
-            ASSERT_EQ(buffer[i], 3);
+        if(rank == 1) {
+            for (int i = 0; i < kol_resursov; i++) {
+                Producer(buffer, kol_elem_in_buffer, rank, i);
+            }
         }
-        std::vector<int> resurce_consume1(kol_resursov, -1);
-        int *resurce_consume;
-        resurce_consume = Create_dinamic_massiv_from_vector(resurce_consume1);
-        for (int i = 0; i < size; i++) {
-            Consumer(buffer, kol_elem_in_buffer, 0, &resurce_consume[i]);
-            ASSERT_EQ(3, resurce_consume[i]);
+        if (rank == 0) {
+            std::vector<int> resurce_consume1(kol_resursov, -1);
+            int *resurce_consume;
+            resurce_consume = Create_dinamic_massiv_from_vector(resurce_consume1);
+            for (int i = 0; i < kol_resursov; i++) {
+                Consumer(buffer, kol_elem_in_buffer, 0, &resurce_consume[i]);
+                ASSERT_EQ(i, resurce_consume[i]);
+            }
         }
     }
 }
