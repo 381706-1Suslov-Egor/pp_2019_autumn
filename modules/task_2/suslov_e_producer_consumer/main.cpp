ab69709c490504b953163637a75f502239925e07
381706-1Suslov-Egor <43166927+381706-1Suslov-Egor@users.noreply.github.com>
2019-11-17 03:33:34 +0300

    producer_consumer

--- modules/task_2/suslov_e_producer_consumer/main.cpp
+++ modules/task_2/suslov_e_producer_consumer/main.cpp
@@ -84,9 +84,7 @@ TEST(Producer_Consumer, Producer_Test3) {
     } else {
         Producer(buffer, kol_elem_in_buffer, 1, 2);
         if (rank == 0) {
-            for (int i = 0; i < kol_resursov; i++) {
-                ASSERT_EQ(buffer[i], 2);
-            }
+            ASSERT_EQ(buffer[0], 2);
         }
     }
 }
@@ -114,7 +112,9 @@ TEST(Producer_Consumer, Consumer_Test1) {
             ASSERT_EQ(1, resurce_consume[i]);
         }
     } else {
-        Producer(buffer, kol_elem_in_buffer, rank, 1);
+        for (int i = 0; i < size; i++) {
+            Producer(buffer, kol_elem_in_buffer, 1, 2);
+        }
         if (rank == 0) {
             for (int i = 0; i < size; i++) {
                 ASSERT_EQ(buffer[i], 2);
@@ -146,7 +146,7 @@ TEST(Producer_Consumer, Consumer_Test2) {
         }
     } else {
         for (int i = 0; i < kol_resursov; i++) {
-            Producer(buffer, kol_elem_in_buffer, 1, 1);
+            Producer(buffer, kol_elem_in_buffer, 1, 3);
         }
         if (rank == 0) {
             std::vector<int> resurce_consume1(kol_resursov, -1);
@@ -154,7 +154,7 @@ TEST(Producer_Consumer, Consumer_Test2) {
             resurce_consume = Create_dinamic_massiv_from_vector(resurce_consume1);
             for (int i = 0; i < kol_resursov; i++) {
                 Consumer(buffer, kol_elem_in_buffer, 0, &resurce_consume[i]);
-                ASSERT_EQ(1, resurce_consume[i]);
+                ASSERT_EQ(3, resurce_consume[i]);
             }
         }
     }
