5b0be3d91a22d6ea536b2e884fc31a2956c1fe01
381706-1Suslov-Egor <43166927+381706-1Suslov-Egor@users.noreply.github.com>
2019-11-21 00:51:34 +0300

    fixiki

--- modules/task_2/suslov_e_producers_consumers/producers_consumers.h
+++ modules/task_2/suslov_e_producers_consumers/producers_consumers.h
@@ -1,6 +1,6 @@
 // Copyright 2019 Suslov Egor
-#ifndef MODULES_TASK_2_SUSLOV_E_PRODUCER_CONSUMER_PRODUCER_CONSUMER_H_
-#define MODULES_TASK_2_SUSLOV_E_PRODUCER_CONSUMER_PRODUCER_CONSUMER_H_
+#ifndef MODULES_TASK_2_SUSLOV_E_PRODUCERS_CONSUMERS_PRODUCERS_CONSUMERS_H_
+#define MODULES_TASK_2_SUSLOV_E_PRODUCERS_CONSUMERS_PRODUCERS_CONSUMERS_H_
 
 #include <vector>
 
@@ -10,4 +10,4 @@ std::vector<int> getRandomVector(int  size);
 int Consumer(int *buffer, int buffer_size, int rank_proc, int* resurce);
 int Producer(int *buffer, int buffer_size, int rank_proc, int resurce);
 
-#endif  // MODULES_TASK_2_SUSLOV_E_PRODUCER_CONSUMER_PRODUCER_CONSUMER_H_
+#endif  // MODULES_TASK_2_SUSLOV_E_PRODUCERS_CONSUMERS_PRODUCERS_CONSUMERS_H_
